54d72555d42b5f244e680a397b0d6ac8a0c4f2b6
elliotb1996 <elliot.blair@my.jcu.edu.au>
2016-08-18 11:39:17 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,11 @@
 """Elliot Blair"""
+
+name = input("What is your name?")
+
+while name == "" or name == " ":
+    print("Name must not be blank")
+    name = input("What is your name?")
+
+print(name)
+
+
